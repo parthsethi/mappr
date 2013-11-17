e618727da6b0a5c6e52b8630b94e3b65e8e2635d
Parth Sethi <parth@parth-VirtualBox.(none)>
2013-11-17 02:23:18 -0500

    add form for the place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,24 @@ class PlacesController < ApplicationController
 
 	def index
 		@place = Place.last
-
 	end
+
+	def new
+		@place = Place.new 
+	end
+
+	def create
+		# Place.create( :title => 'omg', ....)
+		@place = Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+	# user is not allowed to go to private thing, for my use only
+
+	def place_params
+		params.require(:place).permit(:title, :address)
+	end
+
 
 end
