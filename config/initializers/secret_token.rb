7c5a5991afa2d8d622f4b298b91018cae9c32909
Parth Sethi <parth@parth-VirtualBox.(none)>
2013-11-17 01:00:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mappr::Application.config.secret_key_base = '4de54d9b2719a854b30d53ee13eb4ada4d22f4cfc96425655ae8756943d99b7a5e5f298f5ecc26d5e9290af29bce8b92504bba2eedc3a0edd1c4fc5620fbe2cf'
